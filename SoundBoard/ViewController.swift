2ec25f83eeed9b695f2e58b55f6b71dee3f0e6ec
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-05-24 18:02:43 -0500

    Crear Grabaciones y reproducir, listado y reproduccion de audios en CoreData

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tablaGrabaciones: UITableView!
+    
+    var reproducirAudio: AVAudioPlayer?
+    
+    var grabaciones:[Grabacion] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablaGrabaciones.delegate = self
+        tablaGrabaciones.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            grabaciones = try context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        } catch {}
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do {
+            reproducirAudio = try AVAudioPlayer(data: grabacion.audio! as Data)
+            reproducirAudio?.play()
+            print(grabacion)
+        } catch {}
+        tablaGrabaciones.deselectRow(at: indexPath, animated: true)
     }
 
 
